a92f07487b4053a9e13bcca889501289625b293c
grbatinic.bogdan <grbatinic.bogdan@gmail.com>
2018-08-11 17:04:18 +0300

    Add time-entry PUT method. Add header to allow other http methods to middleware

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -29,6 +29,7 @@ const cors = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
     res.header("Access-Control-Expose-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     //res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
     next();
